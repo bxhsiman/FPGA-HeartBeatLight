10d65e57e6f63d792f08915db9a5f143af9be923
siman <siman@qiulishe.com>
2024-03-12 03:40:23 +0800

    [TBD] 需要再加入一个高速时钟用于PWM生成

--- src/led_mode/mode2_breath.v
+++ src/led_mode/mode2_breath.v
@@ -1,52 +1,34 @@
 //呼吸灯驱动
 //create by siman 2024/3/11
-
+[TBD] 再加入一个高速时钟用于PWM生成
 module LED_mode2_driver(
     input clk,
     input rst_n,
-    input [7:0] led_select, // 选择哪个LED灯呼吸
     output reg [7:0] led_out // 8个LED的输出
 );
-    reg [9:0] counter;
-    reg [3:0] brightness;
-    wire [7:0] led_mask;
-
-    // Generate the mask for the selected LED
-    assign led_mask = 8'b00000001 << led_select;
+    reg [9:0] counter = 0; // 用于计数当前LED状态持续的周期数
+    reg [2:0] current_led = 0; // 用于跟踪当前点亮的LED，3位可以表示0到7的范围
+    reg dir; // 用于指示当前LED的亮度变化方向
+    reg [7:0] duty; // 用于指示当前LED的亮度
+    reg [7:0] duty_counter; //占空比计数器
 
     always @(posedge clk or negedge rst_n) begin
         if (~rst_n) begin
-            led_out <= 8'b0;
+            // 异步复位
+            dir <= 1'd0;
             counter <= 10'd0;
-            brightness <= 4'd0;
+            current_led <= 8'd0;
+            led_out <= 8'b0000_0000; // 初始化时所有LED熄灭
         end
         else begin
-            if (counter < 300) begin
-                // First 300 clock cycles, increase brightness
-                if (brightness < 15) begin
-                    brightness <= brightness + 1;
-                end
-            end
-            else if (counter >= 300 && counter < 600) begin
-                // Next 300 clock cycles, decrease brightness
-                if (brightness > 0) begin
-                    brightness <= brightness - 1;
-                end
-            end
-            
-            // Reset counter to loop brightness change
-            if (counter >= 599) begin
-                counter <= 9'd0;
-            end
-            else begin
+            if (counter == 300) begin
+                // 前300个周期内，LED占空比依次增加
+                dir <= 1'd1;
                 counter <= counter + 1;
             end
-
-            // Apply brightness to the selected LED(s)
-            if (brightness > (counter % 16)) begin
-                led_out <= led_mask; // Use mask to light up selected LED
-            end else begin
-                led_out <= 8'd0; // Turn off LEDs not matching the brightness criteria
+            else if (counter == 600) begin
+                dir <= 1'd0;
+                counter <= 10'd0;
             end
         end
     end
