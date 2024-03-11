c57287fcdb8640dee5ab77d9589a5044aff8decc
siman <siman@qiulishe.com>
2024-03-11 08:25:57 +0800

    [rebuild]重构心跳灯MODE1

--- src/led_mode/mode1_heart_beat.v
+++ src/led_mode/mode1_heart_beat.v
@@ -4,22 +4,27 @@
 module LED_mode1_driver (
     input clk,
     input rst_n,
-    output reg led_out 
+    input [3:0] led_select,
+    output reg [7:0] led_out
 );
     reg [6:0] counter;
+    wire [7:0] led_mask; 
+
+    // Generate the mask for the selected LED
+    assign led_mask = 8'b00000001 << led_select;
 
     always @(posedge clk or negedge rst_n) begin
         if(~rst_n) begin
-            led_out <= 1'b0;
+            led_out <= 8'b0;
             counter <= 7'd0;
         end
         else begin
             counter <= counter + 1;
             if (counter == 300) begin //CLK: 600Hz / 2
-                led_out <= ~led_out;
+                // Toggle the selected LED
+                led_out <= (led_out & ~led_mask) | ((~led_out) & led_mask);
                 counter <= 7'd0;
             end
         end
     end 
-
 endmodule
